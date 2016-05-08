eadc8e89840ee0700f98f9faf1346b441b17de8a
wangbing <708861178@qq.com>
2016-05-08 09:08:41 +0800

    1

---
+++ kedashixunDemo/kedashixunDemo/Const.h
@@ -0,0 +1,19 @@
+//
+//  Const.h
+//  LeJi
+//
+//  Created by wangbing on 16/5/7.
+//  Copyright © 2016年 KZL. All rights reserved.
+//
+
+#ifndef Const_h
+#define Const_h
+#define SMS_AppKey @"102d714f16a4e"
+#define SMS_AppSecret @"dc0034e8ab6db42c12bba0c962c839bf"
+
+#define QQ_AppKey @"Pxi3oB0XPqW1nzmT"
+
+#define Sina_AppKey @"3537448415"
+#define Sina_RedirectURI @"https://api.weibo.com/oauth2/default.html"
+
+#endif /* Const_h */
