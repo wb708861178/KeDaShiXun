c681e723702d930ae713f563268dcc180e75158a
KZL <kzl@kzl.local>
2016-05-08 16:49:42 +0800

    no message

--- kedashixunDemo/kedashixunDemo/论坛/Controllers/WBForumViewController.m
+++ kedashixunDemo/kedashixunDemo/论坛/Controllers/WBForumViewController.m
@@ -18,9 +18,6 @@
     [super viewDidLoad];
 
      [self setNavBarTitleWithText:@"我的论坛" withFontSize:20 withTextColor:[UIColor blackColor]];
-    // Do any additional setup after loading the view.
-
-     [self setNavBarTitleWithText:@"科大论坛" withFontSize:20 withTextColor:[UIColor blackColor]];
 
 
 
